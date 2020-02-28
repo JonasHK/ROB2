46474012e196ae2017ad344f5e92a5ff1812fb81
Jonas <jonas.h.kristensen@gmail.com>
2020-02-28 15:27:38 +0100

    Atempt on Exercise_2, but needs to be encapsulated into functions

--- lib/getPose.m
+++ lib/getPose.m
@@ -1,7 +1,6 @@
-function [pose] = getPose()
+function [pose] = getPose(odom)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-odom = rossubscriber('/odom');
 odomdata = receive(odom,3);
 pose = odomdata.Pose.Pose;
 end
