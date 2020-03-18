e2de82f618500d7dc3e308508e6cd81a708639c3
PcFreaker <43938649+Feldbak@users.noreply.github.com>
2020-03-19 00:21:55 +0100

    Point Cloud - Comments

--- Exercise_7/Pointclouds.m
+++ Exercise_7/Pointclouds.m
@@ -1,39 +1,37 @@
 establishConnection()
-%%
+%% Init
 
 close all
 point = rossubscriber('/camera/depth_registered/points');
 
-pointcloud = receive(point,3)
-
-figure
-scatter3(pointcloud);
-%%
+pointcloud = receive(point,3) %Point cloud acquisition
+%% Load Data
 clear
 Vinkelret_1 = load('Vinkelret_1.mat') 
 Vinkelret_2 = load('Vinkelret_2.mat') 
 Vinkel_1 = load('Vinkel_1.mat') 
 Vinkel_2 = load('Vinkel_2.mat') 
 
+%Renaming data in workspace
 Vinkelret_1 = Vinkelret_1.pointcloud
 Vinkelret_2 = Vinkelret_2.pointcloud
 Vinkel_1 = Vinkel_1.pointcloud
 Vinkel_2 = Vinkel_2.pointcloud
 
-%%
-% read XYZ format
+%% Point cloud display
+%read XYZ format
 xyz = Vinkelret_1.readXYZ();
 figure
 scatter3(xyz(:,1),xyz(:,2), xyz(:,3))
 
 xyzc = xyz(~isnan(xyz(:,3)),:); % remove NaN
 
-%% kmeans for clustering
-
+%% Segmented/clustered point cloud (kmeans)
 K = 20;
 [idx,C,sumd] = kmeans(xyzc, K, 'Display', 'iter'); 
 C = C'
 
+%Dots
 figure(1)
 scatter3(xyz(:,1),xyz(:,2), xyz(:,3), '.')
 hold on
@@ -41,6 +39,7 @@ for i=1:K,
     plot3(C(1,i), C(2,i), C(3,i), 'r.')
 end
 
+%Color segmentation
 figure
 for i=1:K
     scatter3(xyzc(idx==i,1),xyzc(idx==i,2), xyzc(idx==i,3), '.')
@@ -60,14 +59,17 @@ for i = 1:K
     N(1,i) = size(P,2); % number points 
 end
 
-%%
+%% Single segment point cloud 
 N_max = 0;
 [N_max, index] = max(N) 
 
 figure
 scatter3(xyzc(idx==index,1),xyzc(idx==index,2), xyzc(idx==index,3), '.')
 
-%% 
+%% Eigenvalue arrays
+
+v=zeros(3,3,20) 
+d=zeros(3,3,20)
 
 x0=zeros(K,3);
 for i = 1:K
@@ -80,14 +82,10 @@ for i = 1:K
     P0 = P - repmat(x0(i,:)', 1, N(i));
     w = ones(1,N(i)); % Weights - could be different..
     J = (repmat(w,3,1).*P0)*P0';
-    J  % show J
+    J;  % show J
 
-    %[v(1,i),d(1,i)] = eig(J);  % eigen-vec/values
+    [v(:,:,i),d(:,:,i)] = eig(J);  % eigen-vec/values
 end
-%%
-  
-
-
 
 %%
 n = v(:,1)'% eigenvector corresponding to lowest eigenvalue..
