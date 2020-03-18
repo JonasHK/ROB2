f88668ae469284b70a7bbbddbfed886d1812bc43
LauritsAndersen <laurits.ha.an@gmail.com>
2020-03-18 11:21:11 +0100

    Opdateret establishedCon. nu med variable IP

--- lib/establishConnection.m
+++ lib/establishConnection.m
@@ -1,17 +1,14 @@
-function [] = establishConnection(simulation)
+function [] = establishConnection(yourIP,turtleIP)
 %establishConnection Establish connection to the robot, and call rostopic
 %list
 %   Detailed explanation goes here
 rosshutdown
+s = strcat('http://192.168.',turtleIP,':11311');
+s1 = strcat('192.168.',yourIP);
 
-if simulation == false
-    setenv('ROS_MASTER_URI','http://192.168.1.200:11311')
-    setenv('ROS_IP','192.168.1.100')
-    rosinit('http://192.168.1.200:11311','NodeHost','192.168.1.100');
-else
-    setenv('ROS_MASTER_URI','http://192.168.1.25:11311')
-    setenv('ROS_IP','192.168.1.23')
-    rosinit('http://192.168.1.25:11311','NodeHost','192.168.1.23');
+setenv('ROS_MASTER_URI',s)
+setenv('ROS_IP',s1)
+rosinit(s,'NodeHost',s1);
 
 rostopic list
 end
