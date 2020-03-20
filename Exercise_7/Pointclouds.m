d8f571953c3b313f55b80b26d455e9cd68df0376
Jonas <jonas.h.kristensen@gmail.com>
2020-03-20 12:37:33 +0100

    Angle measurement on cluster plane

--- Exercise_7/Pointclouds.m
+++ Exercise_7/Pointclouds.m
@@ -1,6 +1,5 @@
 establishConnection()
 %% Init
-
 close all
 point = rossubscriber('/camera/depth_registered/points');
 
@@ -35,7 +34,7 @@ C = C'
 figure(1)
 scatter3(xyz(:,1),xyz(:,2), xyz(:,3), '.')
 hold on
-for i=1:K,
+for i=1:K
     plot3(C(1,i), C(2,i), C(3,i), 'r.')
 end
 
@@ -68,8 +67,8 @@ scatter3(xyzc(idx==index,1),xyzc(idx==index,2), xyzc(idx==index,3), '.')
 
 %% Eigenvalue arrays
 
-v=zeros(3,3,20) 
-d=zeros(3,3,20)
+v=zeros(3,3,20);
+d=zeros(3,3,20);
 
 x0=zeros(K,3);
 for i = 1:K
@@ -86,11 +85,53 @@ for i = 1:K
 
     [v(:,:,i),d(:,:,i)] = eig(J);  % eigen-vec/values
 end
+%% Laurits og Jonas' work
+dwall = zeros(3,3,20);
+figure
+for i = 1:K
+   if d(1,1,i) < 5
+       dwall(:,:,i) = d(:,:,i)
+       %scatter3(dwall(1,1,i), dwall(2,2,i), dwall(3,3,i), '.')
+       %scatter3(dwall)
+       %hold on
+   end
+end
 
 %%
+M = 20; % cluster id
+figure(1)
+scatter3(xyzc(idx==M,1),xyzc(idx==M,2), xyzc(idx==M,3), '.')
+
+P = xyzc(idx==M,:)';    % Plane fitting..
+N = size(P,2); % number points
+
+x0 = mean(P')
+
+figure(2), scatter3(Vinkelret_1)
+
+% SIMILAR TO Plane_fitting_example_CORKE.m
+P0 = P - repmat(x0', 1, N);
+w = ones(1,N); % Weights - could be different..
+J = (repmat(w,3,1).*P0)*P0';
+J  % show J
+
+[v,d] = eig(J)  % eigen-vec/values
+
 n = v(:,1)'% eigenvector corresponding to lowest eigenvalue..
-figure
+figure(3)
 scatter3(P(1,:), P(2,:), P(3,:), '.')
 hold on
 nvec = [x0 ; x0 + 0.5*n]; % normal vector for plotting (from mean to mean+normvec)
-line(nvec(:,1), nvec(:,2), nvec(:,3), 'Color', 'r')
+p0 = [0,0,0];
+p1 = [0,0,2];
+rnvec = [p0;p1];
+
+lineM = line(nvec(:,1), nvec(:,2), nvec(:,3), 'Color', 'r')
+hold on
+lineR = line(rnvec(:,1), rnvec(:,2), rnvec(:,3), 'Color', 'g');
+%%
+vectorM = [p1-p0]; % Robot orientation
+vectorP = [lineM.XData(2)-lineM.XData(1),lineM.YData(2)-lineM.YData(1),lineM.ZData(2)-lineM.ZData(1)];
+
+CosTheta = dot(vectorM,vectorP)/(norm(vectorM)*norm(vectorP));
+ThetaInDegrees = acosd(CosTheta);
