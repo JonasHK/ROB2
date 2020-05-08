1d321a0dd67a0c28e897ca99a26210dbb7e3147d
LauritsAndersen <laurits.ha.an@gmail.com>
2020-05-08 13:03:24 +0200

    Opdateret move med velPub

--- lib/move.m
+++ lib/move.m
@@ -1,9 +1,9 @@
-function [] = move(vel,ang_vel,robot)
+function [] = move(vel,ang_vel,velpub)
 %UNTITLED7 Summary of this function goes here
 %   Detailed explanation goes here
-velmsg = rosmessage(robot);
+velmsg = rosmessage(velpub);
 velmsg.Linear.X = vel;
 velmsg.Angular.Z = ang_vel;
-send(robot,velmsg);
+send(velpub,velmsg);
 end
 
